6ccd5b0b8abe95cb401e7ccbd4e6839fc37361f3
Lei Zheng <zhenglei530@gmail.com>
2024-06-20 15:49:32 +1200

    added TodoList component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,10 @@
+import { TodoList } from './TodoLIst'
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+      <TodoList />
     </div>
   )
 }
