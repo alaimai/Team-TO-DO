65523730ed00362b53c814b7ad2a9710b68442e8
Lei Zheng <zhenglei530@gmail.com>
2024-06-20 14:11:48 +1200

    done create update hooks

--- client/hooks/use-create.ts
+++ client/hooks/use-create.ts
@@ -1,19 +1,20 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQueryClient, useMutation, useQuery } from '@tanstack/react-query'
 import request from 'superagent'
 const API_HOST = 'https://paataka.cloud/api/_/team-todo'
 const TOKEN = 'inboT4cuIWA'
 
-export function useDelete(id: number) {
+export function useCreate() {
   const queryClient = useQueryClient()
-  return useMutation({
+  useMutation({
     mutationFn: async (data) => {
       const res = await request
-        .delete(`${API_HOST}/todo/${id}`)
+        .post(`${API_HOST}/todo`)
+        .send(data)
         .auth(TOKEN, { type: 'bearer' })
       return res.body
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['todos', id] })
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
     },
   })
 }
--- client/hooks/use-update.ts
+++ client/hooks/use-update.ts
@@ -1,14 +1,15 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQueryClient, useMutation, useQuery } from '@tanstack/react-query'
 import request from 'superagent'
 const API_HOST = 'https://paataka.cloud/api/_/team-todo'
 const TOKEN = 'inboT4cuIWA'
 
-export function useDelete(id: number) {
+export function useUpDate(id) {
   const queryClient = useQueryClient()
   return useMutation({
     mutationFn: async (data) => {
       const res = await request
-        .delete(`${API_HOST}/todo/${id}`)
+        .put(`${API_HOST}/todo/${id}`)
+        .send(data)
         .auth(TOKEN, { type: 'bearer' })
       return res.body
     },
